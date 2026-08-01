546873cac6a8a31a65e0e86575315f2c9ef21cf1
ryancnelson <ryan@ted.com>
2026-08-01 02:35:29 -0700

    emulator_loop: include cartridge_layout.h header in emulator_loop.h (308 tests GREEN)

--- src/emulator_loop.h
+++ src/emulator_loop.h
@@ -6,6 +6,7 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include "cartridge_layout.h"
 
 /* Apple II standard timing: 1,023,000 cycles/sec -> ~17,050 cycles per 60Hz frame */
 #define BAOREGON_CYCLES_PER_FRAME 17050u
